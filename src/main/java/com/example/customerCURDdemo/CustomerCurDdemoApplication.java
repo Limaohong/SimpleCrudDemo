7ab75beb50d4cbb2d0da9a0cf81eb6179be5db8c
1302160-NB\Wistronits <n531396008@gmail.com>
2023-09-01 16:06:07 +0800

    first commit

---
+++ src/main/java/com/example/customerCURDdemo/CustomerCurDdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerCURDdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCurDdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCurDdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customerCURDdemo/CustomerCurDdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerCURDdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCurDdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
